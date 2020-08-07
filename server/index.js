247f28bfd582d104e09c24a1a1ba17409b07026d
vproskurin <vladimir.proskurin@gmail.com>
2020-08-06 22:43:16 -0700

    docker for pg and new routes

--- server/index.js
+++ server/index.js
@@ -1,20 +1,52 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const pool = require("./db")
+const pool = require("./db");
 
 // middleware
-app.use(cors())
-app.use(express.json())
+app.use(cors());
+app.use(express.json());
 
 // Routes
 
 // Create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    // const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
 // Get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const allTodos = await pool.query("SELECT * FROM todo");
+    res.json(allTodos.rows);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
 
-
+// Update todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [
+      id,
+    ]);
+    res.json(todo.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+// Delete todo
 
 app.listen(5000, () => {
-    console.log("server has started on port 5000")
-})
+  console.log("server has started on port 5000");
+});

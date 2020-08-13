35f8fe15073c5f90a8bc5504b2cf0fa339416ea0
vproskurin <vladimir.proskurin@gmail.com>
2020-08-12 19:41:34 -0700

    editing done

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,25 +1,37 @@
 import React, { Fragment, useEffect, useState } from "react";
+import EditTodo from "./EditTodo";
 
 const ListTodos = () => {
+  const [todos, setTodos] = useState([]);
 
-  const [todos, setTodos] = useState([])
+  // Delete Todo function
+
+  const deleteTodo = async (id) => {
+    try {
+      const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+        "method": "DELETE"
+      });
+
+      setTodos(todos.filter(todo => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err.message)
+    }
+  }
 
   const getTodos = async () => {
     try {
-
       const response = await fetch("http://localhost:5000/todos");
       const jsonData = await response.json();
 
-      setTodos(jsonData)
-
+      setTodos(jsonData);
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
   };
 
   useEffect(() => {
     getTodos();
-  },[]);
+  }, []);
 
   console.log(todos);
   return (
@@ -34,19 +46,20 @@ const ListTodos = () => {
           </tr>
         </thead>
         <tbody>
-          {/*<tr>
-            <td>John</td>
-            <td>Doe</td>
-            <td>john@example.com</td>
-          </tr>
-            */}
-            {todos.map(todo => (
-              <tr>
-                <td>{todo.description}</td>
-                <td>edit</td>
-                <td>delete</td>
-              </tr>
-            ))}
+          {todos.map((todo) => (
+            <tr key={todo.todo_id}>
+              <td>{todo.description}</td>
+              <td><EditTodo todo={todo} /></td>
+              <td>
+                <button
+                  className="btn btn-danger"
+                  onClick={() => deleteTodo(todo.todo_id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </Fragment>

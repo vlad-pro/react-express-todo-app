8b0c7d534b33bcbf03fe3d64b9f1df7009bbe7bd
vproskurin <vladimir.proskurin@gmail.com>
2020-09-23 18:58:45 -0700

    ct privacy page added and old auth page moved to new folder

---
+++ update/privacy.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.master')
+@section('title','Privacy Policy')
+
+@section('content')
+    <div class="position-relative u-gradient-half-primary-v1 u-space-3-top u-space-4-bottom rounded-top-pseudo px-7 px-md-9">
+        <div class="container">
+            <h1 class="text-white font-weight-bold">Privacy Policy</h1>
+            <p class="u-text-light">Last modified: August 29, 2018</p>
+        </div>
+
+        <figure class="position-absolute-bottom-0">
+            <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                 width="100%" height="140px" viewBox="20 -20 300 100" style="margin-bottom: -8px;" xml:space="preserve">
+              <path class="u-fill-white" opacity="0.4" d="M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729
+            c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z" />
+              <path class="u-fill-white" opacity="0.4" d="M-35.667,44.628c0,0,42.91-34.463,87.51-14.191c77.31,35.141,113.304-1.952,146.639-4.729
+            c48.653-4.055,69.939,16.218,69.939,16.218v54.396H-35.667V44.628z" />
+              <path class="u-fill-white" opacity="0" d="M43.415,98.342c0,0,48.283-68.927,109.133-68.927c65.886,0,97.983,67.914,97.983,67.914v3.716
+            H42.401L43.415,98.342z" />
+              <path class="u-fill-white" d="M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428
+            c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z" />
+            </svg>
+        </figure>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12">
+
+                <h4>General Principles</h4>
+
+                <p>
+                    ChainTrack (CT) is aware of your legitimate concerns about privacy and is fully committed to maintaining the integrity, confidentiality, security, and accuracy of your Personal Information in accordance with applicable privacy legislation and best industry practices.
+                </p>
+
+                <p>
+                    This Privacy Policy (the "Policy") helps you understand the guiding principles applied by us to the collection, use, disclosure, retention, and disposal of and access to Personal Information on this site.
+                </p>
+
+                <p>
+                    Because privacy concerns and legal and business requirements evolve continually, CT may have to change its Policy at any time without advance notice and therefore you are invited to review it regularly. Access to and use of this site are also governed by CT site terms of use that you should review.
+                </p>
+
+                <p>
+                    By using this website and providing us with your <strong>Personal Information</strong>, you agree that CT uses the latter in accordance with the Privacy Act and this Policy. For purposes of this Policy, "Personal Information" means any information about an identifiable individual within the meaning of the Privacy Act.
+                </p>
+
+                <h4>Guiding Principles</h4>
+
+                <p>
+                    Our Policy rests on the following seven guiding principles, identifying the purposes of use. The nature and scope of the Personal Information we need depends to a great extent on how products, solutions or services are of interest to you. CT will collect Personal Information only in relation to its services for demonstration and business purposes only.
+                </p>
+
+                <h4>Obtaining Your Consent</h4>
+
+                <p>
+                    Your Consent can be express or implied ("Consent"). In our website, It is expressed in writing, or electronically. Your Consent can be implied through your action or inaction. For instance, when you register in our website, we conclude that you have consented to the collection, use, and disclosure of the Personal Information it contains for the intended purposes. The same conclusion may be drawn when you use one of solutions or services. Your Consent can also be given by your authorized representative, for instance, if he/she is providing Personal Information and is seeking services or solutions from our website. Your Personal Information will be viewed by CT representatives for the purposes of their work or for which it was given.
+                </p>
+
+                <h4>Limiting Collection, Use, and Disclosure of Personal Information</h4>
+
+                <p>
+                    CT will not, without your Consent, collect, use, or disclose Personal Information except for the purposes identified for the user.
+                </p>
+
+                <h4>Providing Access and Accuracy</h4>
+
+                <p>
+                    Subject to legal requirements provided by the Privacy Act or the Access to Information Act, CT will respect your right of access to your Personal Information by informing you of its procedures and requirements for responding to your request for access.
+                </p>
+
+                <h4>Responding to Your Privacy Concerns</h4>
+
+                <p>
+                    CT will respond to your questions and investigate diligently any complaints with respect to your Personal Information.
+                </p>
+
+                <h4>Responding to Your Concerns</h4>
+
+                <p>For questions or comments regarding this Policy, for additional information about the management of Personal Information, or to consult or modify your Personal Information contact us by email at <a href="mailto:legal@chaintrack.ca">legal@chaintrack.ca</a>.</p>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ update/terms.blade.php
@@ -0,0 +1,200 @@
+@extends('layouts.master')
+@section('title','Terms of Use')
+
+@section('content')
+    <div class="position-relative u-gradient-half-primary-v1 u-space-3-top u-space-4-bottom rounded-top-pseudo px-7 px-md-9">
+        <div class="container">
+            <h1 class="text-white font-weight-bold">Terms and Conditions</h1>
+            <p class="u-text-light">Last modified: August 29, 2018</p>
+        </div>
+
+        <figure class="position-absolute-bottom-0">
+            <svg preserveAspectRatio="none" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                 width="100%" height="140px" viewBox="20 -20 300 100" style="margin-bottom: -8px;" xml:space="preserve">
+              <path class="u-fill-white" opacity="0.4" d="M30.913,43.944c0,0,42.911-34.464,87.51-14.191c77.31,35.14,113.304-1.952,146.638-4.729
+            c48.654-4.056,69.94,16.218,69.94,16.218v54.396H30.913V43.944z" />
+              <path class="u-fill-white" opacity="0.4" d="M-35.667,44.628c0,0,42.91-34.463,87.51-14.191c77.31,35.141,113.304-1.952,146.639-4.729
+            c48.653-4.055,69.939,16.218,69.939,16.218v54.396H-35.667V44.628z" />
+              <path class="u-fill-white" opacity="0" d="M43.415,98.342c0,0,48.283-68.927,109.133-68.927c65.886,0,97.983,67.914,97.983,67.914v3.716
+            H42.401L43.415,98.342z" />
+              <path class="u-fill-white" d="M-34.667,62.998c0,0,56-45.667,120.316-27.839C167.484,57.842,197,41.332,232.286,30.428
+            c53.07-16.399,104.047,36.903,104.047,36.903l1.333,36.667l-372-2.954L-34.667,62.998z" />
+            </svg>
+        </figure>
+    </div>
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-lg-12">
+                <p>
+                    The following describes the terms in which CHAIN TRACK, this website {{ config('app.url') }} or its subdomains, herein referred to as “CHAIN TRACK TECHNOLOGIES INC.”, “us”, “we” “our”, and this “website” provides you access to our services. We reserve the right to supplement, revise, or update these terms at any time without notice to you. Be advised that you should read carefully these terms, the Privacy Policy and as well as our Refund Policy on a regular basis as you continue to use our website because they may change at any time. Continued use of this website will indicate your acceptance of our terms, policies, updates, revisions and conditions so please check back and refer to this page often.
+                </p>
+
+                <p>
+                    Users agrees to review this Agreement prior to first sign up and agrees to the terms and conditions outlined in this online Agreement with respect to the goods, services and information provided by or through the website. This Agreement constitutes the entire and only contract between CHAIN TRACK and the users, and supersedes any and all prior or contemporaneous agreements, representations, warranties, and understandings with respect to the goods, services and information provided by or through the website, and the subject matter of this Agreement.
+                </p>
+
+                <h4>General Conditions</h4>
+
+                <p>
+                    I. This license agreement is non-transferable and non-commercial. This means the user, after paying applicable monthly or annual subscriptions in full, can use the tool solely for the application and usage as outlined in the Privacy Policy and Terms of Use, but prevents the licensee to use it for commercial purposes and services to the third party.
+                </p>
+
+                <p>
+                    II. Limited Warranty - The Software is provided “AS IS”. CHAIN TRACK TECHNOLOGIES INC. does not warrant that the operation of the Software will be uninterrupted or error free, or that the functions contained in the Software will meet Licensee's requirements. VAT MAKES NO REPRESENTATIONS, WARRANTIES OR INDEMNITIES, WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE, RESPECTING THIS LICENSE OR THE SOFTWARE, AND ALL WARRANTIES ARISING FROM COURSE OF DEALING OR USAGE OF TRADE ARE HEREBY EXCLUDED.
+                </p>
+
+                <p>
+                    III. Limitation of Liability - IN NO EVENT WILL VAT BE LIABLE FOR ANY LOST REVENUE, PROFIT, OR DATA, OR FOR SPECIAL, DIRECT, INDIRECT, CONSEQUENTIAL, INCIDENTAL, OR PUNITIVE DAMAGES HOWEVER CAUSED AND REGARDLESS OF THE THEORY OF LIABILITY ARISING OUT OF THE USE OF OR INABILITY TO USE THE SOFTWARE EVEN IF VATA HAS BEEN ADVISED OF THE POSSIBILITY OF SUCH DAMAGES. IN NO EVENT SHALL KEMA’S TOTAL LIABILITY EXCEED THE LICENSE FEE PAID TO VAT BY LICENSEE.
+                </p>
+
+                <p>
+                    IV. Force Majeure - VAT shall neither be deemed in default of any obligation hereunder nor be liable for any delay or failure in performance which results directly or indirectly from Acts of God, acts of civil or military authorities, civil disturbance, war, strikes, fires or other causes beyond reasonable control
+                </p>
+
+                <h4>Additional Consitions</h4>
+
+                <ul class="text-muted">
+                    <li>I. Licensee may not delegate any of its obligations or assign any of its rights or remedies hereunder.  Any such attempted delegation or assignment shall be null and void.</li>
+                    <li>II. The waiver by either party of, or failure of either party to exercise in any respect, any right provided herein shall not be deemed a waiver of such right in the future or of any other right hereunder.</li>
+                    <li>III. Licensee agrees to pay VAT all costs and expenses including reasonable attorneys’ fees incurred by VAT in exercising any of its rights or remedies.</li>
+                    <li>IV. The terms and conditions of this License constitute the complete and exclusive statement of the agreement between the parties with respect to the licensing of the Software and supersedes and merges all prior and contemporaneous proposals, representations, statements understandings or agreements, written or oral, express or implied.  Licensee acknowledges that it did not enter into this License in reliance upon any representation by VAT or understanding by Licensee that is not set forth herein.</li>
+                    <li>V. Should any provision or section of this License be void, unenforceable for any reason or unlawful, then that provision or section shall be deemed severable from this License and shall not affect the enforceability or validity of any of the remaining provisions or sections of the License or the License as a whole.</li>
+                    <li>VI. Software Support is provided to Licensee by VAT under agreed above terms, conditions and pricing.</li>
+                    <li>VII. Modifications to the Software may only be made by or in consultation with VAT. Cost of modification and consulting services thereon shall be provided to Licensee by VAT under separate terms, conditions and pricing.</li>
+                    <li>VIII. The Software will access VAT database of energy storage technologies and applications as well as other technical parameters that are specific to users’ needs.</li>
+                </ul>
+
+                <h4>Eligibility For Users</h4>
+
+                <p>
+                    This website is available only to individuals over 18 years of age and/or businesses that can form legally binding contracts under applicable law. Anyone using our services must have the authority to bind the buyers or sellers to a contract. If you do not meet all of these eligibility requirements, please do not use the website. We reserve the right to disapprove, curtail or terminate your use of the website at any time, for any reason and in our sole discretion without notice to you. YOUR USE OF THE WEBSITE SHALL CONSTITUTE YOUR ACCEPTANCE OF THIS AGREEMENT IN ITS ENTIRETY.
+                </p>
+
+                <h4>User Responsibilities and Duties</h4>
+
+                <p>
+                    We request that you alert us to any actions that interfere with the integrity of our service. If you suspect illegal activities, price or offer manipulation, fraud or multiple identities, it is important to provide us with this information immediately.
+                </p>
+
+                <h4>Disclaimer</h4>
+
+                <p>
+                    Our site acts as the venue for which registered users track their reefer tracking devices. Our website only facilitate the communication between users and devices.
+                </p>
+
+                <p>
+                    We reserve the right, but not the obligation, to modify or cancel registeration where it appears that a customer has engaged in fraudulent, illegal, or other inappropriate activity, and to prohibit or restrict your participation or utilization of the site if you fail to comply with all Terms and Conditions contained herein as determined in our sole discretion.
+                </p>
+
+                <h4>User Information</h4>
+
+                <p>
+                    “Your Information” is defined as any information you provide to us or other users in the registration, bidding or service listing process, in any public message area (including the feedback area) or through any email feature. You are solely responsible for Your Information, and we act as a passive conduit for your online distribution and publication of Your Information.
+                </p>
+
+                <p>
+                    With respect to your information (or any items listed therein):
+                </p>
+
+                <ul class="text-muted">
+                    <li>(1) shall not be false, inaccurate or misleading;</li>
+                    <li>(2) shall not be fraudulent;</li>
+                    <li>(3) shall not infringe any third party’s copyright, patent, trademark, trade secret or other proprietary rights or rights of publicity or privacy;</li>
+                    <li>(4) shall not violate any law, statute, ordinance or regulation (including without limitation those governing export control, consumer protection, unfair competition, anti discrimination or false advertising);</li>
+                    <li>(5) shall not be defamatory, unlawfully threatening or unlawfully harassing;</li>
+                    <li>(6) shall not contain any viruses, Trojan horses, worms, time bombs, cancel bots or other computer programming routines that are intended to damage, detrimentally interfere with, surreptitiously intercept or expropriate any system, data or personal information;</li>
+                    <li>(7) shall not create liability for us or cause us to lose (in whole or in part) the services of our ISPs (Internet Service Providers) or other suppliers.</li>
+                </ul>
+
+                <p>
+                    In order to enable CHAIN TRACK to use the personal information you supply us with, so that we are not violating any rights you might have in that information, you agree to grant us a non-exclusive, worldwide, perpetual, irrevocable, royalty-free, sub licensable (through multiple tiers) right to exercise the copyright and publicity rights (but no other rights) you have in Your Information, in any media now known or not currently known, with respect to personal information. CHAIN TRACK will only use personal information in accordance with our Privacy Policy.
+                </p>
+
+                <h4>Breach, Suspension or Termination</h4>
+
+                <p>
+                    Without limiting other remedies, CHAIN TRACK may limit your activity, issue a warning, temporarily suspend, indefinitely suspend or terminate your membership and refuse to provide our services to you if:
+                </p>
+
+                <ul class="text-muted">
+                    <li>(a) your breach of this Agreement or the documents it incorporates by reference;</li>
+                    <li>(b) our inability to verify or authenticate any information you provide to us;</li>
+                    <li>(c) your provision of fraudulent information to us or our users;</li>
+                    <li>(d) if you have interfered with the integrity of the functioning of our site, including but not limited to price manipulation; or</li>
+                    <li>(e) our belief that your actions may cause legal liability for you, our users or us in its sole discretion, deems such remedy necessary for any reason.</li>
+                </ul>
+
+                <p>
+                    If we choose to ignore any violation of this Agreement or other activities which could give rise to suspension or termination, such action or inaction on our part will not constitute a waiver of any of our rights including suspension, termination or otherwise.
+                </p>
+
+                <h4>Trademarks and Copyrights</h4>
+
+                <p>
+                    The trademarks, algorithms, service marks and logos used and displayed on the website are claimed, registered, or unregistered trademarks, copyright and service marks of CHAIN TRACK. You are not granted, expressly or by implication, estoppel or otherwise, any license or right to use any trademark, service mark or logo used or displayed on the website, without the express written permission of us. CHAIN TRACK disclaims any proprietary interest in trademarks, service marks, logos, domain names, and trade names other than its own.
+                </p>
+
+                <h4>No Warranty</h4>
+
+                <p>
+                    CHAIN TRACK AND ITS TRACKING OR OTHER SERVICES ARE “AS IS” AND WITHOUT ANY WARRANTY OR CONDITION, EXPRESS, IMPLIED OR STATUTORY. WE AND OUR AFFILIATES SPECIFICALLY DISCLAIM ANY IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT.
+                </p>
+
+                <h4>Liability Limit</h4>
+
+                <p>
+                    IN NO EVENT SHALL CHAIN TRACK OR ITS STAFF BE LIABLE FOR LOST PROFITS OR ANY SPECIAL, INCIDENTAL OR CONSEQUENTIAL DAMAGES ARISING OUT OF OR IN CONNECTION WITH OUR SITE, OUR SERVICES OR THIS AGREEMENT (HOWEVER ARISING, INCLUDING NEGLIGENCE).
+                </p>
+
+                <h4>Indemnity</h4>
+
+                <p>
+                    You agree to indemnify and hold us harmless, our subsidiaries, affiliates, related parties, officers, directors, employees, agents, independent contractors, advertisers, partners, and co-branders from any claim or demand, including reasonable attorney’s fees, that may be made by any third party, that is due to or arising out of your conduct or connection with this website or service, your provision of content, your breach of this Terms of Service or any other violation of the rights of another person or party.
+                </p>
+
+                <h4>Legal Compliance</h4>
+
+                <p>
+                    You shall comply with all applicable domestic and international laws, statutes, ordinances and regulations regarding your use of our service and your offers on, listing, purchase, solicitation of offers to purchase, and sale of items.
+                </p>
+
+                <h4>No Agency</h4>
+
+                <p>
+                    You and CHAIN TRACK are independent entities, and no agency, partnership, joint venture, employee-employer or franchiser-franchisee relationship is intended or created by this Agreement.
+                </p>
+
+                <h4>Notices</h4>
+
+                <p>
+                    Except as explicitly stated otherwise, any notices to us shall be given by postal mail provided at the bottom of this page or by the email address you provide to CHAIN TRACK during the registration process (in your case). Notice shall be deemed given 2-3 business days after email is sent, unless the sending party is notified that the email address is invalid. Alternatively, we may give you notice by certified mail, Notices via Canada Post, DHL or other major mail channel shall be deemed given 3 days after the date of mailing.
+                </p>
+
+                <h4>Govering Law and Jurisdiction</h4>
+
+                <p>
+                    This Agreement shall be governed by and construed in accordance with the laws of the Province of British Columbia and the federal laws of Canada applicable therein. You hereby expressly consent to the sole and exclusive jurisdiction and venue of the courts of the Province of British Columbia, for any legal proceeding arising out of or relating to your use of this website or this Agreement. We do not guarantee continuous, uninterrupted or secure access to our services, and operation of our website may be interfered with by numerous factors outside of our control.
+                </p>
+
+                <p>
+                    Use of the CHAIN TRACK website is unauthorized in any jurisdiction that does not give effect to all provisions of these terms and conditions including, without limitation, this paragraph. Our performance of this Agreement is subject to existing laws and legal process, and nothing contained in this Agreement is in derogation of our right to comply with governmental, court and law enforcement requests or requirements relating to your use of the CHAIN TRACK web sites or information provided to or gathered by us with respect to such use. If any part of this Agreement is determined to be invalid or unenforceable pursuant to applicable law including, but not limited to, the warranty disclaimers and liability limitations set forth above, then the invalid or unenforceable provision will be deemed superseded by a valid, enforceable provision that most closely matches the intent of the original provision and the remainder of the Agreement shall continue in effect. Unless specifically provided herein, this Agreement constitutes the entire Agreement between you and CHAIN TRACK with respect to your use of this website. This Agreement shall supersede all prior Agreements, understanding, negotiations and discussions, either oral or written between the parties. There are no representations, warranties, conditions or other agreements, express or implied, statutory or otherwise, between the parties in connection with the use of this website except as specifically set forth herein.
+                </p>
+
+                <h4>Your Acceptance of these terms</h4>
+
+                <p>
+                    By using this Site, you signify your acceptance of this policy and terms of service. If you do not agree to this policy, please do not use our Site. Your continued use of the Site following the posting of changes to this policy will be deemed your acceptance of those changes.
+                </p>
+
+                <h4>Contact Us</h4>
+
+                <p>
+                    If you have any questions about this agreement, the practices of this site, or your dealings with this site, please contact us by email, phone or mail listed below.
+                </p>
+
+                @include('pages._address')
+            </div>
+        </div>
+    </div>
+@endsection
